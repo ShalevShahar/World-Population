6dfe97385c8c4ce5101d507722b1eacb8e0bd389
Shalev <shalev_v@walla.co.il>
2020-03-28 22:00:24 +0300

    Cool black dashbaord

--- live-streaming/flask-live-streaming/flaskBasic.py
+++ live-streaming/flask-live-streaming/flaskBasic.py
@@ -30,20 +30,63 @@ df['popRate'] = df['popGap'] / timeGap20192020
 # APP SERVER + connection to HTML
 app = Flask(__name__)
 
-@app.route('/_popStream', methods=['GET'])
-def popStream():
+
+@app.route('/_allCountries', methods=['GET'])
+def allCountries():
         dateNow = time.time()
         timePassed = (int(dateNow) - timeJuly2019)
         df['popNow'] =(df['pop2019'] + df['popRate'] * timePassed).astype(int)
-        china = df['popNow'][40].astype(int)
-        return jsonify(china = china)
+        df1 = df.set_index('Location').loc[:, 'popNow']
+        df1 = df1.sort_values(axis=0, ascending =False)
+        allCountries = df1.to_json()
+        return  allCountries     
+   
+   
+
+
+ 
+@app.route('/_topCountries', methods=['GET'])
+def topCountries():
+        dateNow = time.time()
+        timePassed = (int(dateNow) - timeJuly2019)
+        df['popNow'] =(df['pop2019'] + df['popRate'] * timePassed).astype(int)
+        
+        #calculating pop minus 1 second
+        timePassedMinusOne = int(timePassed) - 2
+        df['popNowMinusSecond'] =(df['pop2019'] + df['popRate'] * timePassedMinusOne).astype(int)
+        
+        df1 = df.set_index('Location').loc[:, ['popNow', 'popNowMinusSecond']]
+        df1 = df1.sort_values(axis=0, ascending =False, by='popNow').head(20)
+        
+        topCountries = df1.to_json(orient='index')
+        print(timePassedMinusOne)
+        return  topCountries    
+
+
 
 
 @app.route('/')
 def index():
         return render_template('PopulationStreaming.html')
 
-
 if __name__ == '__main__':
     app.run(debug=True)       
     
+    
+# @app.route('/test')
+# def index2():
+#     if not request.script_root:
+#         # this assumes that the 'index' view function handles the path '/'
+#         request.script_root = url_for('index', _external=True)
+
+#     return render_template('PopulationStreaming.html')
+
+
+# @app.route('/_china', methods=['GET'])
+# def popStream():
+#         dateNow = time.time()
+#         timePassed = (int(dateNow) - timeJuly2019)
+#         df['popNow'] =(df['pop2019'] + df['popRate'] * timePassed).astype(int)
+#         china = df['popNow'][40].astype(int)
+#         china = int(china)
+#         return jsonify(china = china)
--- live-streaming/flask-live-streaming/static/runningCounter.js
+++ live-streaming/flask-live-streaming/static/runningCounter.js
@@ -24,103 +24,168 @@ async function currentPopCalc() {
     const obj = document.getElementById("c");
     obj.innerHTML = parseInt(popNow).toLocaleString();
     //repitition time in milisecs of the function 
-    setTimeout(currentPopCalc, 100000);
+    setTimeout(currentPopCalc, 1000);
 }
 currentPopCalc()
 
 
 
-// var intervalID = setInterval(update_values, 1000);
+$(document).ready(function() {
+    var intervalID = setInterval(update_top_countries, 2000);
 
-// function update_values() {
-//     $.getJSON($SCRIPT_ROOT + '/_popStream',
+    function update_top_countries() {
+        $.getJSON('http://127.0.0.1:5000' + '/_topCountries',
+            function(data) {
+                var countryData = '';
+                $.each(data, function(key, value) {
+                    countryData += '<tr>';
+                    if (value.popNow > value.popNowMinusSecond) {
+                        countryData += '<td>' + key + '</td>';
+                        countryData += '<td style="border-style: solid; border-width: 0.1px; border-color: green;">' + value.popNow.toLocaleString() + '</td>';
+                    } else if (value.popNow < value.popNowMinusSecond) {
+                        countryData += '<<td>' + key + '</td>';
+                        countryData += '<<td style="color: red;">' + value.popNow.toLocaleString() + '</td>';
+                    } else {
+                        countryData += '<td>' + key + '</td>';
+                        countryData += '<td>' + value.popNow.toLocaleString() + '</td>';
+                    }
+                    countryData += '</tr>';
+
+                    // var table = document.getElementById("topCountries")
+                    // for (var i = 0, cell; cell = table.cells[i]; i++) {
+                    // if (value.popNow > value.popNowMinusSecond) {
+                    //     function changeElement() {
+                    //         countryData += '<td style="background-color: yellow;">' + key + '</td>';
+                    //         // var el = document.getElementsByTagName('td');
+                    //         // el.style.color = "red";
+                    //         // el.style.fontSize = "15px";
+                    //         // el.style.backgroundColor = "White";
+                    //     }
+                    //     changeElement()
+                    //     console.log(key, value.popNow)
+                    // }
+
+                })
+
+                document.getElementById("topCountries").innerHTML = countryData;
+            });
+
+    };
+});
+
+
+
+var intervalID = setInterval(update_country_pop, 1000);
+
+function update_country_pop() {
+    $.getJSON('http://127.0.0.1:5000' + '/_allCountries',
+        function(data) {
+            var countryData = '';
+            $.each(data, function(key, value) {
+                countryData += '<tr>';
+                countryData += '<td>' + key + '</td>';
+                countryData += '<td>' + value.toLocaleString() + '</td>';
+                countryData += '</tr>';
+
+            })
+
+
+            //$("tableCountries").animateHighlight("#dd0000", 1000);
+            document.getElementById("tableCountries").innerHTML = countryData;
+
+        });
+};
+
+
+// var intervalID = setInterval(update_world_pop, 1000);
+
+// function update_world_pop() {
+//     $.getJSON('http://127.0.0.1:5000' + '/_china',
 //         function(data) {
 //             $('#china').text(data.china);
-//             console.log(data)
+//             document.getElementById("china").innerHTML;
 //         });
 // };
 
+//html += "</tr></table>";
+
+// ATTACH HTML TO CONTAINER
+//document.getElementById("tableCountries").innerHTML = html;
+
+// //function - per country world population
+// async function currentPopByCountry() {
+//     //fetching the file and converting it to text
+
+//     const response = await fetch('/static/countries2019-2020.csv');
+//     let data1 = await response.text();
+//     data1Splitted = data1.split('\n')
+
+
+//     const timeJuliFirst2019 = 1561939200
+//     const dateNow = Math.floor(new Date().getTime() * 0.001);
+
+//     const allCountriesNames = []
+//     const allCountriesPopNow = []
+//     const gap20192020 = (1593561600 - 1561939200)
+//     for (i = 1; i < 236; i++) {
+//         countryData = data1Splitted[i].split(',')
+//         allCountriesNames.push(countryData[0])
+//         CountryPop2019 = countryData[1];
+//         CountryPop2020 = countryData[2];
+//         CountryPopDiff = CountryPop2020 - CountryPop2019
+//         CountryPopRate = parseFloat(CountryPopDiff / gap20192020)
+//         CountyPopNow = parseFloat(CountryPop2019) + (parseFloat(CountryPopRate) * (parseFloat(dateNow) - parseFloat(timeJuliFirst2019)))
+//         allCountriesPopNow.push(parseInt(CountyPopNow).toLocaleString())
+//     }
 
 
 
+//     // const obj = document.getElementById("countriesId");
+//     // obj.innerHTML = parseInt(filteredCountries).toLocaleString();
+
+//     window.addEventListener("load", function() {
+//         // LET'S SAY THAT WE HAVE A SIMPLE FLAT ARRAY
+//         // DRAW THE HTML TABLE
+//         var perrow = 1, // 3 items per row
+//             html = "<table><tr>";
+
+//         // Loop through array and add table cells
+//         for (var i = 0; i < allCountriesNames.length; i++) {
+//             html += "<td>" + allCountriesNames[i] + "</td>";
+//             // Break into next row
+//             var next = i + 1;
+//             if (next % perrow == 0 && next != allCountriesNames.length) {
+//                 html += "</tr><tr>";
+//             }
+//         }
+//         html += "</tr></table>";
+
+//         // ATTACH HTML TO CONTAINER
+//         document.getElementById("container").innerHTML = html;
+
+//     });
 
 
+//     window.addEventListener("load", function() {
+//         // LET'S SAY THAT WE HAVE A SIMPLE FLAT ARRAY
+//         // DRAW THE HTML TABLE
+//         var perrow = 1, // 3 items per row
+//             html = "<table><tr>";
 
+//         // Loop through array and add table cells
+//         for (var i = 0; i < allCountriesPopNow.length; i++) {
+//             html += "<td>" + allCountriesPopNow[i] + "</td>";
+//             // Break into next row
+//             var next = i + 1;
+//             if (next % perrow == 0 && next != allCountriesPopNow.length) {
 
-//function - per country world population
-async function currentPopByCountry() {
-    //fetching the file and converting it to text
+//                 html += "</tr><tr>";
+//             }
+//         }
+//         html += "</tr></table>";
 
-    const response = await fetch('/static/countries2019-2020.csv');
-    let data1 = await response.text();
-    data1Splitted = data1.split('\n')
-
-
-    const timeJuliFirst2019 = 1561939200
-    const dateNow = Math.floor(new Date().getTime() * 0.001);
-
-    const allCountriesNames = []
-    const allCountriesPopNow = []
-    const gap20192020 = (1593561600 - 1561939200)
-    for (i = 1; i < 236; i++) {
-        countryData = data1Splitted[i].split(',')
-        allCountriesNames.push(countryData[0])
-        CountryPop2019 = countryData[1];
-        CountryPop2020 = countryData[2];
-        CountryPopDiff = CountryPop2020 - CountryPop2019
-        CountryPopRate = parseFloat(CountryPopDiff / gap20192020)
-        CountyPopNow = parseFloat(CountryPop2019) + (parseFloat(CountryPopRate) * (parseFloat(dateNow) - parseFloat(timeJuliFirst2019)))
-        allCountriesPopNow.push(parseInt(CountyPopNow).toLocaleString())
-    }
-
-
-
-    // const obj = document.getElementById("countriesId");
-    // obj.innerHTML = parseInt(filteredCountries).toLocaleString();
-
-    window.addEventListener("load", function() {
-        // LET'S SAY THAT WE HAVE A SIMPLE FLAT ARRAY
-        // DRAW THE HTML TABLE
-        var perrow = 1, // 3 items per row
-            html = "<table><tr>";
-
-        // Loop through array and add table cells
-        for (var i = 0; i < allCountriesNames.length; i++) {
-            html += "<td>" + allCountriesNames[i] + "</td>";
-            // Break into next row
-            var next = i + 1;
-            if (next % perrow == 0 && next != allCountriesNames.length) {
-                html += "</tr><tr>";
-            }
-        }
-        html += "</tr></table>";
-
-        // ATTACH HTML TO CONTAINER
-        document.getElementById("container").innerHTML = html;
-
-    });
-
-
-    window.addEventListener("load", function() {
-        // LET'S SAY THAT WE HAVE A SIMPLE FLAT ARRAY
-        // DRAW THE HTML TABLE
-        var perrow = 1, // 3 items per row
-            html = "<table><tr>";
-
-        // Loop through array and add table cells
-        for (var i = 0; i < allCountriesPopNow.length; i++) {
-            html += "<td>" + allCountriesPopNow[i] + "</td>";
-            // Break into next row
-            var next = i + 1;
-            if (next % perrow == 0 && next != allCountriesPopNow.length) {
-
-                html += "</tr><tr>";
-            }
-        }
-        html += "</tr></table>";
-
-        // ATTACH HTML TO CONTAINER
-        document.getElementById("container2").innerHTML = html;
-    });
-}
-currentPopByCountry()
+//         // ATTACH HTML TO CONTAINER
+//         document.getElementById("container2").innerHTML = html;
+//     });
+// }
+// currentPopByCountry()
